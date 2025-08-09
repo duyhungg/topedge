6b9ce33baffb0539b0622da9070f3b51e710c49e
duyhungg <dongocduyhung7@gmail.com>
2025-08-09 10:15:49 +0700

    ++upcode

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,30 @@
 import { Button } from "@/components/ui/button";
-
+import { ButtonLoading } from "@/components/custom/ButtonLoading";
+import { CardDemo } from "@/components/custom/CardDemo";
 export default function HomePage() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
     <div>
-      <div className="mx-2">
-        <h1>Home Page</h1>
-        <Button>Click me</Button>
+      <div className="bg-red-600 flex items-center justify-center h-screen gap-4">
+        {/* Button với các props khác nhau */}
+        <Button>Default Button</Button>
+
+        <Button variant="destructive" size="lg">
+          Large Destructive
+        </Button>
+
+        <Button variant="outline" size="sm">
+          Small Outline
+        </Button>
+
+        <Button variant="ghost" disabled>
+          Disabled Ghost
+        </Button>
+        <CardDemo></CardDemo>
+        <ButtonLoading></ButtonLoading>
       </div>
     </div>
   );
