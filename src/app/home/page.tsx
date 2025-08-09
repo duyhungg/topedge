5ecd6e4cab27636a83ebe0a9f1bf3c99bda7c7c8
duyhungg <dongocduyhung7@gmail.com>
2025-08-09 09:57:57 +0700

    ++ add shadcn ui

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div>
+      <div className="mx-2">
+        <h1>Home Page</h1>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+}
